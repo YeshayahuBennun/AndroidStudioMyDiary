e6d8b13253c2a9d497e2eed6323923dc721292fc
YeshayahuBennun <ybennun@yahoo.com>
2021-10-26 18:57:42 +0300

    commit#10 -  My Diary App - Delete a Diary

--- app/src/main/java/com/ybennun/mydiary/DiaryAdapter.kt
+++ app/src/main/java/com/ybennun/mydiary/DiaryAdapter.kt
@@ -1,18 +1,24 @@
 package com.ybennun.mydiary
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.provider.BaseColumns._ID
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.ybennun.mydiary.data.DatabaseManager.DiaryEntry.TABLE_NAME
 import com.ybennun.mydiary.data.Diary
+import com.ybennun.mydiary.data.DiaryDBHelper
+import kotlinx.android.synthetic.main.recycler_diary_item.view.*
 
 
 class DiaryAdapter(private var diaryList: MutableList<Diary>) :
     RecyclerView.Adapter<DiaryAdapter.DiaryViewHolder>() {
 
-    override fun onCreateViewHolder(viewGroup: ViewGroup, p1: Int): DiaryViewHolder {
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onCreateViewHolder(viewGroup: ViewGroup, position: Int): DiaryViewHolder {
         val context = viewGroup.context
         val inflater = LayoutInflater.from(context)
         val shouldAttachToParentImmediately = false
@@ -23,6 +29,20 @@ class DiaryAdapter(private var diaryList: MutableList<Diary>) :
             shouldAttachToParentImmediately
         )
 
+        view.delete_button.setOnClickListener {
+            val mDBHelper = DiaryDBHelper(view.context)
+
+            val db = mDBHelper.writableDatabase
+
+            val selection = "$_ID = ?"
+            val selectionArgs = arrayOf("${(diaryList[position].id)}")
+
+            db.delete(TABLE_NAME, selection, selectionArgs)
+
+            diaryList.removeAt(position)
+
+            notifyDataSetChanged()
+        }
         return DiaryViewHolder(view)
     }
 
@@ -43,8 +63,8 @@ class DiaryAdapter(private var diaryList: MutableList<Diary>) :
 
         override fun onClick(v: View?) {
             val context = itemView.context
-            val intent = Intent(context,NewDiary::class.java)
-            intent.putExtra("IDofRow",diary.id)
+            val intent = Intent(context, NewDiary::class.java)
+            intent.putExtra("IDofRow", diary.id)
             context.startActivity(intent)
         }
 
